99a0c25124eba2f76e50fc6b4f480c1f8a559389
Thiện Khải <thienkhaiak5555@gmail.com>
2022-11-08 08:03:51 +0700

    add header, footer, devide folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Footer from './Common/Footer/Footer';
+import SignIn from './Pages/Sign In/SignIn/SignIn';
+import Home from './Pages/Manager/Home_tier1/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Pages/Manager/Home_tier1/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Header from '../Header_Manager/Header'
+
+const Home = () => {
+    return (
+        <div className='ABC'>
+            <Header />
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
