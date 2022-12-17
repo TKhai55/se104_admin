be0381009bdba45de2059db22b8ebb7be61871bd
ziljnk <duy0000linh0000@gmail.com>
2022-12-17 12:50:19 +0700

    add detail results Linh

--- src/Pages/Manager/AddFixtures/AddFixtures.jsx
+++ src/Pages/Manager/AddFixtures/AddFixtures.jsx
@@ -4,20 +4,23 @@ import Header from '../Header_Manager/Header'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const rounds = ['Vòng 1', 'Vòng 2', 'Vòng 3']
 
 const AddFixtures = () => {
-    const [fixtures, setFixtures] = useState(null)
+    const rounds = ['Vòng 1', 'Vòng 2', 'Vòng 3']
+    let [fixtures, setFixtures] = useState([])
+    const getFixtures = async () => {
+        try {
+            const res = await axios.get('http://localhost:8000/v1/trandau/read')
+            setFixtures(res.data)
+            fixtures = res.data
+            console.log(fixtures)
+        }
+        catch (error) {
+            console.log(error.message)
+        }
+    }
     useEffect(() => {
-        // async function fetchData() {
-            axios.defaults.baseURL = 'http://localhost:8000/'
-            axios.get('/v1/trandau/read').then(res => setFixtures(res.data.trandau))
-            // const {trandau} = respond.data
-            
-           console.log(fixtures);
-
-        // }
-        // fetchData()
+        getFixtures()
     }, [])
 
   return (
@@ -48,85 +51,28 @@ const AddFixtures = () => {
                                     <th>{round} / {rounds.length}</th>
                                 </tr>
                                 {
-                                    fixtures.length > 0 && fixtures.trandau.map((fixtureItem, index) => {
+                                     fixtures.map((fixtureItem, index) => {
+                                        const imageUrl1 = 'http://localhost:8000/' + fixtureItem.DOI1.LOGO
+                                        const imageUrl2 = 'http://localhost:8000/' + fixtureItem.DOI2.LOGO
                                         return (
                                             <tr className='round-info' key={index}>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-1" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Hoang_Anh_Gia_Lai_FC_logo.svg/1200px-Hoang_Anh_Gia_Lai_FC_logo.svg.png" alt="Hoang Anh Gia Lai Logo" name="logo-1" />
-                                        <label htmlFor="logo-1" id="logo-1">{fixtureItem.DOI1.TENCLB}</label>
-                                    </td>
-                                    <td id="time">
-                                        <h4>{fixtureItem.THOIGIANDIENRA}</h4>
-                                        <h4 id="date">{fixtureItem.NGAYDIENRA}</h4>
-                                    </td>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-2" src="https://upload.wikimedia.org/wikipedia/vi/thumb/2/21/H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg/1200px-H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg.png" alt="Hai Phong Logo" name="logo-2"/>
-                                        <label htmlFor="logo-2" id="logo-2">{fixtureItem.DOI2.TENCLB}</label>
-                                    </td>
-                                </tr>
+                                                <td className='signature'>
+                                                    <img className="club-logo club-logo-1" src={imageUrl1} alt={`${fixtureItem.DOI1.TENCLB} logo`} name="logo-1" />
+                                                    <label htmlFor="logo-1" id="logo-1">{fixtureItem.DOI1.TENCLB}</label>
+                                                </td>
+                                                <td id="time">
+                                                    <h4>{fixtureItem.THOIGIANDIENRA}</h4>
+                                                    <h4 id="date">{fixtureItem.NGAYDIENRA}</h4>
+                                                </td>
+                                                <td className='signature'>
+                                                    <img className="club-logo club-logo-2" src={imageUrl2} alt={`${fixtureItem.DOI2.TENCLB} logo`} name="logo-2"/>
+                                                    <label htmlFor="logo-2" id="logo-2">{fixtureItem.DOI2.TENCLB}</label>
+                                                </td>
+                                            </tr>
                                         )
                                     })
                                 }
 
-                                {/* <tr className='round-info'>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-1" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Hoang_Anh_Gia_Lai_FC_logo.svg/1200px-Hoang_Anh_Gia_Lai_FC_logo.svg.png" alt="Hoang Anh Gia Lai Logo" name="logo-1" />
-                                        <label htmlFor="logo-1" id="logo-1">{fixtures.DOI1}</label>
-                                    </td>
-                                    <td id="time">
-                                        <h4>17:00</h4>
-                                        <h4 id="date">Th 6, 30/09/2022</h4>
-                                    </td>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-2" src="https://upload.wikimedia.org/wikipedia/vi/thumb/2/21/H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg/1200px-H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg.png" alt="Hai Phong Logo" name="logo-2"/>
-                                        <label htmlFor="logo-2" id="logo-2">{fixtures.DOI2}</label>
-                                    </td>
-                                </tr>
-
-                                <tr className='round-info'>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-1" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Hoang_Anh_Gia_Lai_FC_logo.svg/1200px-Hoang_Anh_Gia_Lai_FC_logo.svg.png" alt="Hoang Anh Gia Lai Logo" name="logo-1" />
-                                        <label htmlFor="logo-1" id="logo-1">Hoang Anh Gia Lai</label>
-                                    </td>
-                                    <td id="time">
-                                        <h4>17:00</h4>
-                                        <h4 id="date">Th 6, 30/09/2022</h4>
-                                    </td>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-2" src="https://upload.wikimedia.org/wikipedia/vi/thumb/2/21/H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg/1200px-H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg.png" alt="Hai Phong Logo" name="logo-2"/>
-                                        <label htmlFor="logo-2" id="logo-2">Hai Phong</label>
-                                    </td>
-                                </tr>
-
-                                <tr className='round-info'>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-1" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Hoang_Anh_Gia_Lai_FC_logo.svg/1200px-Hoang_Anh_Gia_Lai_FC_logo.svg.png" alt="Hoang Anh Gia Lai Logo" name="logo-1" />
-                                        <label htmlFor="logo-1" id="logo-1">Hoang Anh Gia Lai</label>
-                                    </td>
-                                    <td id="time">
-                                        <h4>17:00</h4>
-                                        <h4 id="date">Th 6, 30/09/2022</h4>
-                                    </td>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-2" src="https://upload.wikimedia.org/wikipedia/vi/thumb/2/21/H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg/1200px-H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg.png" alt="Hai Phong Logo" name="logo-2"/>
-                                        <label htmlFor="logo-2" id="logo-2">Hai Phong</label>
-                                    </td>
-                                </tr>
-
-                                <tr className='round-info'>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-1" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Hoang_Anh_Gia_Lai_FC_logo.svg/1200px-Hoang_Anh_Gia_Lai_FC_logo.svg.png" alt="Hoang Anh Gia Lai Logo" name="logo-1" />
-                                        <label htmlFor="logo-1" id="logo-1">Hoang Anh Gia Lai</label>
-                                    </td>
-                                    <td id="time">
-                                        <h4>17:00</h4>
-                                        <h4 id="date">Th 6, 30/09/2022</h4>
-                                    </td>
-                                    <td className='signature'>
-                                        <img className="club-logo club-logo-2" src="https://upload.wikimedia.org/wikipedia/vi/thumb/2/21/H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg/1200px-H%E1%BA%A3i_Ph%C3%B2ng_FC_2021.svg.png" alt="Hai Phong Logo" name="logo-2"/>
-                                        <label htmlFor="logo-2" id="logo-2">Hai Phong</label>
-                                    </td>
-                                </tr> */}
                             </table>
                         )
                     })
---
+++ src/Pages/Manager/DetailAddResult/ConfirmDeleteResult/ConfirmDeleteResult.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ConfirmDeleteResult.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+function ConfirmDeleteResult({openModal, onClose, onConfirm}) {
+  if (!openModal) return null
+    return (
+    <div className='overlay' onClick={onClose}>
+        <div className='modal-container'>
+            <p>{`Bạn có chắc chắn muốn xoá sự kiện này?`}</p>
+            <div className='button-container'>
+                <button className='confirm-button' onClick={onConfirm}>Có <FontAwesomeIcon icon={faCheck}/></button>
+                <button className='cancle-button' onClick={onClose}>Huỷ <FontAwesomeIcon icon={faXmark}/></button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ConfirmDeleteResult
